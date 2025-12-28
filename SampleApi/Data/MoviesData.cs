8593ec105e83940ba464a6a8f2a85bf444c060d0
Arkamit <arkamitdutta@gmail.com>
2025-12-28 13:42:08 +0530

    Creating and Managing Movie Endpoints

---
+++ SampleApi/Data/MoviesData.cs
@@ -0,0 +1,22 @@
+﻿using SampleApi.Models;
+using System.Text.Json;
+
+namespace SampleApi.Data;
+
+public class MoviesData
+{
+    public List<MoviesModel> Movies { get; private set; }
+
+    public MoviesData()
+    {
+        var options = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
+
+        string filePath = Path.Combine(Directory.GetCurrentDirectory(), "Data", "moviesdata.json");
+        string jsonData = File.ReadAllText(filePath);
+
+        Movies = JsonSerializer.Deserialize<List<MoviesModel>>(jsonData, options) ?? new();
+    }
+}
