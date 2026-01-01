04a517425820da6f2fd592cefc3c27df2f13c0af
Arkamit <arkamitdutta@gmail.com>
2026-01-01 21:17:44 +0530

    Adding API Test project using Blazor and setting up CORS support

--- SampleApi/Startup/OpenApiConfig.cs
+++ SampleApi/Startup/OpenApiConfig.cs
@@ -3,7 +3,7 @@ namespace SampleApi.Startup;
 
 public static class OpenApiConfig
 {
-    public static void AppOpenApiServices(this IServiceCollection services)
+    public static void AddOpenApiServices(this IServiceCollection services)
     {
         services.AddOpenApi();
     }
---
+++ SampleTestUI/Models/MovieModel.cs
@@ -0,0 +1,13 @@
+﻿namespace SampleTestUI.Models;
+
+public class MovieModel
+{
+    public int Id { get; set; }
+    public required string Title { get; set; }
+    public required string Genre { get; set; }
+    public int ReleaseYear { get; set; }
+    public float Rating { get; set; }
+    public required string Director { get; set; }
+    public int Duration { get; set; }
+    public required string Description { get; set; }
+}
